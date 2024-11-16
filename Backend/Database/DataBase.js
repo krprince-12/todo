6f4efd90ea7f908ec2771b633c3ae4320b5d4457
krprince-12 <144045878+krprince-12@users.noreply.github.com>
2024-11-16 16:10:36 +0530

    Update DataBase.js

--- Backend/Database/DataBase.js
+++ Backend/Database/DataBase.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// Load environment variables from .env file
 dotenv.config();
-
-// MongoDB URI from environment variables (MongoDB Atlas or Local)
 const mongoURI = process.env.DB_URI ;
 
-// Function to connect to MongoDB
+
 const connectDB = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
-    process.exit(1);  // Exit the process with failure code
+    process.exit(1);  
   }
 };
 
-// Export the connection function
+
 module.exports = connectDB;
